0833c203e6e0f93632cc4cd95c2fbd10108eb0f5
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-10-22 13:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-//your JS code here. If required.
 let textInputVal = document.getElementById('text');
 let numberInputVal = document.getElementById('delay');
 let submitBtn = document.getElementById('btn');
+let outputDiv = document.getElementById('output');
 
-submitBtn.addEventListner('click' , (e)=>{
-	e.prevnetDefault();
-	function test1(){
-		let textVal = textInputVal.value;
-		setTimeout(()=>{
-			textVal.innerText = "Test - 1";
-		},1000)
+async function displayMessageWithDelay(text, delay) {
+    await new Promise((resolve) => setTimeout(resolve, delay));
+    outputDiv.innerText = text;
+}
 
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-		function test2(){
-		let numberVal = numberInputVal.value;
-		setTimeout(()=>{
-			numberInputVal.innerText = "Test - 2";
-		},2000)
-		
-	}
-		test1();
-		test2();
-})
+    let textVal = textInputVal.value;
+    let delayVal = Number(numberInputVal.value);
+
+    if (textVal && delayVal > 0) {
+        displayMessageWithDelay(textVal, delayVal);
+    } else {
+        outputDiv.innerText = "Please enter valid text and delay.";
+    }
+});
