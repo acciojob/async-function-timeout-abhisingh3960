cb1b1260f15f111a8a854cc891d95d6948f3f056
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-10-22 13:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let textInputVal = document.getElementById('text');
+let numberInputVal = document.getElementById('delay');
+let submitBtn = document.getElementById('btn');
+
+submitBtn.addEventListner('click' , (e)=>{
+	e.prevnetDefault();
+	function test1(){
+		let textVal = textInputVal.value;
+		setTimeout(()=>{
+			textVal.innerText = "Test - 1";
+		},1000)
+
+
+		function test2(){
+		let numberVal = numberInputVal.value;
+		setTimeout(()=>{
+			numberInputVal.innerText = "Test - 2";
+		},2000)
+		
+	}
+		test1();
+		test2();
+})
